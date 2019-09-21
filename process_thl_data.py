8fe80214a2426381f15d8588b3dae4099fb10432
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2019-09-21 18:27:45 +0300

    Add some data from thl

---
+++ process_thl_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+raw_df = pd.read_csv("fact_ahil_pikarap01.csv", sep=";")
+providers = raw_df.Palveluntuottaja.value_counts().index.sort_values()
+
+weeks_by_provider = (raw_df[raw_df.Palveluntuottaja == key].val for key in providers)
+data = np.stack(weeks_by_provider)
+week_list = list(raw_df[raw_df.Palveluntuottaja == providers[0]].Viikko)
+week_list = map(lambda x: "{}_{}".format(x[1], 2018 if x[0] <= 51 else 2019), enumerate(week_list))
+df = pd.DataFrame(data, index=providers, columns=week_list)
+df.to_csv("thl_data.csv")
